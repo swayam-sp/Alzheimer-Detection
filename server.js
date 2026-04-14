2c6771c8773af1105fd959dcfc6fafa16c509ffb
swayam-sp <swayampawarssp2005@gmail.com>
2026-04-14 19:29:19 +0530

    Setup with env Config

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const path = require('path');
 const { spawn } = require('child_process');
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://ismdncztwtubmgplzmim.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzbWRuY3p0d3R1Ym1ncGx6bWltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0Mzg1MjEsImV4cCI6MjA3NjAxNDUyMX0.pGP3b0T5FYJK2HPJU7aBz6ZY73fWnC728u03KKDGFSs';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
